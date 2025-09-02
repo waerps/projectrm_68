e24296763540f4541c7c3c897076dc96cab7e787
Gamontip Gongpet <gamontipgongpet@Gamontips-MacBook-Pro-3.local>
2025-09-02 14:22:32 +0700

    v1

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/", //router
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/p",
+    element: <App />,
+  },
+]);
+
+
+
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  //   <App />
+  //   <App />
+  // </StrictMode>,
+  <RouterProvider router={router} />,
+)
